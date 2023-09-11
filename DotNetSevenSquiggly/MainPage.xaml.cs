23283cc5d98c591f5bcca2a3aa7bc8c5f2d20c6f
SalmanBaris25 <100187581+SalmanBaris25@users.noreply.github.com>
2023-09-11 14:29:12 +0530

    Add files via upload

---
+++ DotNetSevenSquiggly/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using Path = Microsoft.Maui.Controls.Shapes.Path;
+
+namespace DotNetSevenSquiggly
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            AddPath();
+        }
+
+        void AddPath()
+        {
+            Path squigglyPath = CreateSquigglyPath(Colors.Green, 1, new Rect(0, 0, 924.445556640625, 22.079999923706055));
+            MainLayout.SetLayoutBounds(squigglyPath, new Rect(0, 0, 924.445556640625, 22.079999923706055));
+            MainLayout.Add(squigglyPath);
+        }
+
+
+        Path CreateSquigglyPath(Color color, double opacity, Rect bounds)
+        {
+            Path squigglyPath = GetSquigglyGeometry(
+                new Point(bounds.X, bounds.Y + bounds.Height),
+                new Point(bounds.X + bounds.Width, bounds.Y + bounds.Height),
+                bounds.Height);
+            squigglyPath.Stroke = color.MultiplyAlpha((float)opacity);
+#if !WINDOWS && !MACCATALYST
+            squigglyPath.StrokeThickness = 0.5;
+#endif
+            return squigglyPath;
+        }
+
+        private Path GetSquigglyGeometry(Point startPoint, Point endPoint, double height)
+        {
+            double dx = startPoint.X - endPoint.X;
+            double dy = startPoint.Y - endPoint.Y;
+            double length = Math.Sqrt(dx * dx + dy * dy);
+            double x = startPoint.X;
+            double y = startPoint.Y;
+            var pathGeometry = new PathGeometry();
+            var pathFigure = new PathFigure();
+            LineSegment lineSegment;
+            pathFigure.StartPoint = startPoint;
+            pathFigure.IsClosed = false;
+            bool showUnderlineAtStart = false;
+            double spacing = height * 0.13;
+            Path squigglyPath = new Path();
+            List<Point> squigglyPointsCollection = new List<Point>();
+            for (double distance = 0; distance + spacing < length; distance += spacing)
+            {
+                if (showUnderlineAtStart)
+                {
+                    lineSegment = new LineSegment();
+                    lineSegment.Point = new Point(x + distance + spacing, y);
+                }
+                else
+                {
+                    lineSegment = new LineSegment();
+                    lineSegment.Point = new Point(x + distance + spacing, y - spacing);
+                }
+                squigglyPointsCollection.Add(new Point(lineSegment.Point.X, lineSegment.Point.Y));
+                showUnderlineAtStart = !showUnderlineAtStart;
+                pathFigure.Segments.Add(lineSegment);
+            }
+            pathGeometry.Figures.Add(pathFigure);
+            squigglyPath.Data = pathGeometry;
+            return squigglyPath;
+        }
+    }
+}
---
+++ DotNetSevenSquiggly/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DotNetSevenSquiggly
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
